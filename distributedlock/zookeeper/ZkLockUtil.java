6f9d9e9971e63c002b76f88cf1adf65bcc5d7fea
pjiu <1712869177@qq.com>
2021-02-09 16:25:18 +0800

    Add files via upload

---
+++ distributedlock/zookeeper/ZkLockUtil.java
@@ -0,0 +1,41 @@
+package com.seckill.distributedlock.zookeeper;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
+
+public class ZkLockUtil{
+	
+	@Value("${zookeeper.address}")
+	private static String address;
+	
+	public static CuratorFramework client;
+	
+	static{
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3); 
+        client = CuratorFrameworkFactory.newClient(address, retryPolicy); 
+        client.start();
+	}
+	
+    //获得了锁
+    public static void acquire(String lockKey){
+    	try {
+    		InterProcessMutex mutex = new InterProcessMutex(client, "/curator/lock/"+lockKey); 
+    		mutex.acquire();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    //释放锁
+    public static void release(String lockKey){
+    	try {
+    		InterProcessMutex mutex = new InterProcessMutex(client, "/curator/lock/"+lockKey); 
+    		mutex.release();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}  
